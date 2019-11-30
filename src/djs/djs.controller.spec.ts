922b5a16df6e735710f8732192abab5e59d03d46
Mathias54 <4n0ny2015>
2019-11-30 03:23:48 -0300

    - Adiciona exemplo de teste e2e e teste unitário.

---
+++ src/djs/djs.controller.spec.ts
@@ -0,0 +1,29 @@
+import { DjsController } from './djs.controller';
+
+
+describe('Unit: DJ Controller', () => {
+  let djController;
+  let djService;
+  let songService;
+
+  beforeEach(() => {
+    djService = {
+      getDj: jest.fn(),
+    };
+    songService = jest.fn();
+
+    djController = new DjsController(
+      djService,
+      songService,
+    );
+  });
+
+  describe('o método', () => {
+    describe('findDjById()', () => {
+      it('deve chamar getDj com ID passado via parâmetro', () => {
+        djController.findDjById('mockID');
+        expect(djService.getDj).toHaveBeenCalledWith('mockID');
+      });
+    });
+  });
+});
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,23 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { DjsModule } from "../src/djs/djs.module";
+import { DjsService } from "../src/djs/djs.service";
 
-describe('AppController (e2e)', () => {
+describe('DJ Controller (e2e)', () => {
   let app;
+  const result = {
+    id: 1,
+    name: 'Reactivation',
+    artists: ['Aura Vortex'],
+    genre: ['Trance', 'Psy Trance'],
+    releaseYear: 2017,
+  };
+  let djProvider = { getDj: () => Promise.resolve(result) };
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [ DjsModule ],
+    }).overrideProvider(DjsService)
+      .useValue(djProvider)
+      .compile();
 
-    app = moduleFixture.createNestApplication();
+    app = module.createNestApplication();
     await app.init();
   });
 
   it('/ (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/djs/1')
+      .expect(result);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 });
